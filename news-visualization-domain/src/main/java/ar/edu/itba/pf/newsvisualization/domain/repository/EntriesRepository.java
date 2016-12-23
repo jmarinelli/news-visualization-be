16ef876a09d6c13f48ea5f28b67ca6979f4668c5
Lucas Moscovicz <lmoscovicz@gmail.com>
2016-12-23 15:49:25 -0300

    Reduced medias and categories

--- news-visualization-domain/src/main/java/ar/edu/itba/pf/newsvisualization/domain/repository/EntriesRepository.java
+++ news-visualization-domain/src/main/java/ar/edu/itba/pf/newsvisualization/domain/repository/EntriesRepository.java
@@ -13,9 +13,9 @@ public interface EntriesRepository extends CrudRepository<Entry, String> {
     @Query(value = "SELECT COUNT(*), e.tmp, e.seccion FROM entries e WHERE e.source LIKE '%news.google.com%' GROUP BY e.tmp, e.seccion HAVING e.tmp in ?1")
     List<Object[]> getMediaCategories(List<String> medias);
 
-    @Query(value = "SELECT DISTINCT(tmp) FROM entries")
+    @Query(value = "SELECT DISTINCT(tmp) FROM entries WHERE source LIKE '%news.google.com%'")
     List<String> getMedia();
 
-    @Query(value = "SELECT DISTINCT(seccion) FROM entries")
+    @Query(value = "SELECT DISTINCT(seccion) FROM entries WHERE source LIKE '%news.google.com%'")
     List<String> getCategories();
 }
