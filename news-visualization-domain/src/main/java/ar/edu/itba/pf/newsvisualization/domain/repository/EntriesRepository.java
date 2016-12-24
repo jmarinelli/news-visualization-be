ee602def8709a35b472a6db6c89f2cf4e9e33b88
Lucas Moscovicz <lmoscovicz@gmail.com>
2016-12-24 16:40:20 -0300

    Changed in radars + jar

--- news-visualization-domain/src/main/java/ar/edu/itba/pf/newsvisualization/domain/repository/EntriesRepository.java
+++ news-visualization-domain/src/main/java/ar/edu/itba/pf/newsvisualization/domain/repository/EntriesRepository.java
@@ -13,7 +13,7 @@ public interface EntriesRepository extends CrudRepository<Entry, String> {
     @Query(value = "SELECT COUNT(*), e.tmp, e.seccion FROM entries e WHERE e.source LIKE '%news.google.com%' GROUP BY e.tmp, e.seccion HAVING e.tmp in ?1")
     List<Object[]> getMediaCategories(List<String> medias);
 
-    @Query(value = "SELECT DISTINCT(tmp) FROM entries WHERE source LIKE '%news.google.com%'")
+    @Query(value = "SELECT tmp FROM entries WHERE source LIKE '%news.google.com%' GROUP BY tmp HAVING COUNT(*) > 3 ORDER BY COUNT(*) DESC")
     List<String> getMedia();
 
     @Query(value = "SELECT DISTINCT(seccion) FROM entries WHERE source LIKE '%news.google.com%'")
